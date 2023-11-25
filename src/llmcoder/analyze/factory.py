487ffbfcc78d55db20ee6b7b0b36718d9c4efcc2
psaegert <paulsaegert@protonmail.com>
2023-11-25 17:16:17 +0100

    Remove leftover files

--- src/llmcoder/analyze/factory.py
+++ src/llmcoder/analyze/factory.py
@@ -23,14 +23,14 @@ class AnalyzerFactory:
             The created analyzer
         """
         match analyzer:
-            case "api_documentation_analyzer_v1":
-                from llmcoder.analyze.APIDocumentationAnalyzer import APIDocumentationAnalyzer
-                return APIDocumentationAnalyzer(*args, **kwargs)
-            case "syntax_analyzer_v1":
-                from llmcoder.analyze.SyntaxAnalyzer import SyntaxAnalyzer
-                return SyntaxAnalyzer(*args, **kwargs)
-            case "unit_test_analyzer_v1":
-                from llmcoder.analyze.UnitTestAnalyzer import UnitTestAnalyzer
-                return UnitTestAnalyzer(*args, **kwargs)
+            # case "api_documentation_analyzer_v1":
+            #     from llmcoder.analyze.APIDocumentationAnalyzer import APIDocumentationAnalyzer
+            #     return APIDocumentationAnalyzer(*args, **kwargs)
+            # case "syntax_analyzer_v1":
+            #     from llmcoder.analyze.SyntaxAnalyzer import SyntaxAnalyzer
+            #     return SyntaxAnalyzer(*args, **kwargs)
+            # case "unit_test_analyzer_v1":
+            #     from llmcoder.analyze.UnitTestAnalyzer import UnitTestAnalyzer
+            #     return UnitTestAnalyzer(*args, **kwargs)
             case _:
                 raise ValueError("Invalid analyzer name")
