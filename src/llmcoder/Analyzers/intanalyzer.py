74817b163037d2775469e2ff00695ee02676e406
Kushal Gaywala <kushalgaywala@gmail.com>
2023-11-27 02:44:39 +0100

    Completed APIDocumentationAnalyzer
    - Covered the edge cases mentioned in issue #42

--- src/llmcoder/Analyzers/intanalyzer.py
+++ src/llmcoder/Analyzers/intanalyzer.py
@@ -1,25 +1,36 @@
-# Interface for definition of suggestions' analysis. 
-# Inherited by 
+# Interface for definition of suggestions' analysis.
+# Inherited by
 # 1. SyntaxAnalyzer
 # 2. UnitTestAnalyzer
 # 3. APIDocumentationAnalyzer
 
 """
-Python further deviates from other languages in one other aspect. 
+Python further deviates from other languages in one other aspect.
 It doesn’t require the class that’s implementing the interface to define all of the interface’s abstract methods.
 """
 
-import os
 from abc import ABCMeta, abstractmethod
 
 
 # Interface to follow the Duck Typing principle supported by Python
 # TODO: be defined a wrapper for the BaseDecorator
-class FormalAnalyzerInterface(metaclass = ABCMeta):
-    
-    # Required method for the analyzer to be implemented in concrete component
-    @abstractmethod
-    def analyze(self, code_suggestion: str) -> str:
-        pass
-    
+class FormalAnalyzerInterface(metaclass=ABCMeta):
+    def __init__(self) -> None:
+        self.code_suggestion = ''
 
+    # Setter method for code_suggestion
+    @classmethod
+    def set_code_suggestion(self, code_suggestion: str) -> None:
+        self.code_suggestion = code_suggestion
+
+    # Getter method for code_suggestion
+    @classmethod
+    def get_code_suggestion(self) -> str:
+        return self.code_suggestion
+
+    # Required method for the analyzer to be implemented in concrete component
+    # Can return an error from unittest,syntaxtest: str, return docs: list[dict[str, str]] from APIDocs, return None for nothing
+    @abstractmethod
+    def analyze(self, code_suggestion: str) -> str | list[dict[str, str]] | None:
+        self.set_code_suggestion(code_suggestion)
+        raise NotImplementedError("Analyze abstract method is not implemented!")
