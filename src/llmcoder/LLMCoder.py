3fb8ef6517db303a089493f35f6dcc46da7a2c64
psaegert <paulsaegert@protonmail.com>
2023-12-08 18:27:23 +0100

    Support multi choice generation

--- src/llmcoder/LLMCoder.py
+++ src/llmcoder/LLMCoder.py
@@ -3,6 +3,8 @@ import os
 from datetime import datetime
 
 import openai
+import torch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 from llmcoder.analyze.factory import AnalyzerFactory
 from llmcoder.utils import get_conversations_dir, get_openai_key, get_system_prompt, get_system_prompt_dir
@@ -17,7 +19,7 @@ class LLMCoder:
                  system_prompt: str | None = None,
                  max_iter: int = 10,
                  log_conversation: bool = True,
-                 temperature: float = 0.7):
+                 device: str | torch.device | None = None):
         """
         Initialize the LLMCoder
 
@@ -37,34 +39,50 @@ class LLMCoder:
             The maximum number of iterations to run the feedback loop, by default 10
         log_conversation : bool, optional
             Whether to log the conversation, by default False
-        temperature : float, optional
-            The temperature to use for the completion, by default 0.7
+        device : str | torch.device | None, optional
+            The device to use for the scoring model, by default the first available GPU or CPU
         """
+        # Check for invalid feedback variants
+        if feedback_variant not in ["separate", "coworker"]:
+            raise ValueError("Inavlid feedback method")
+        self.feedback_variant = feedback_variant
+
+        # Remember the models
+        self.model_first = model_first
+        self.model_feedback = model_feedback
+
+        # Set the feedback loop variables
+        self.iterations = 0
+        self.analyzer_pass_history: list[list[dict]] = []
+        self.analyzer_message_history: list[list[dict]] = []
+        self.max_iter = max_iter
+        self.messages: list = []
+
+        # Set up the analyzers
         if analyzers is None:
             self.analyzers = []
         else:
             self.analyzers = [AnalyzerFactory.create_analyzer(analyzer) for analyzer in analyzers]
 
-        self.model_first = model_first
-        self.model_feedback = model_feedback
+        # Set up the scoring model
+        if device is None:
+            self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        elif isinstance(device, str):
+            self.device = torch.device(device)
 
+        self.completion_score_tokenizer = AutoTokenizer.from_pretrained("microsoft/CodeBERT-base")
+        self.completion_score_model = AutoModelForSequenceClassification.from_pretrained("microsoft/CodeBERT-base").to(self.device)
+
+        # Set up the OpenAI API
         self.client = openai.OpenAI(api_key=get_openai_key())
-        if feedback_variant not in ["separate", "coworker"]:
-            raise ValueError("Inavlid feedback method")
 
-        self.iterations = 0
-        self.analyzer_pass_history: list[list[dict]] = []
-        self.analyzer_message_history: list[list[dict]] = []
-        self.max_iter = max_iter
-        self.feedback_variant = feedback_variant
-
+        # Set up logging
         if log_conversation:
             self.conversation_file = self._create_conversation_file()
         else:
             self.conversation_file = None  # type: ignore
 
-        self.messages: list = []
-
+        # Get the system prompt
         if system_prompt is None:
             self.system_prompt = get_system_prompt()
         elif system_prompt in os.listdir(get_system_prompt_dir()):
@@ -72,11 +90,27 @@ class LLMCoder:
         else:
             self.system_prompt = system_prompt
 
-        self.temperature = temperature
-
+        # Add the system prompt to the messages
         self._add_message("system", message=self.system_prompt)
 
-    def complete(self, code: str) -> str:
+    def to(self, device: str | torch.device) -> None:
+        """
+        Move the scoring model to the specified device
+
+        Parameters
+        ----------
+        device : str | torch.device
+            The device to use
+        """
+        if isinstance(device, str):
+            device = torch.device(device)
+        elif isinstance(device, torch.device):
+            self.device = device
+        else:
+            raise TypeError("Invalid device type")
+        self.completion_score_model = self.completion_score_model.to(self.device)
+
+    def complete(self, code: str, temperature: float = 0.7, n: int = 14) -> str:
         """
         Complete the provided code with the LLMCoder feedback loop
 
@@ -84,6 +118,10 @@ class LLMCoder:
         ----------
         code : str
             The code to complete
+        temperature : float, optional
+            The temperature to use for the completion, by default 0.7
+        n : int, optional
+            The number of choices to generate, by default 14
 
         Returns
         -------
@@ -92,14 +130,14 @@ class LLMCoder:
         """
         # Get the first completion with
         print("Creating first completion...")
-        self.complete_first(code)
+        self.complete_first(code, temperature, n)
 
         print("Starting feedback loop...")
         if len(self.analyzers) > 0:
             # Run the feedback loop until the code is correct or the max_iter is reached
             for i in range(self.max_iter):
                 print(f"Starting feedback iteration {i + 1}...")
-                if self.feedback_step():
+                if self.feedback_step(temperature, n):
                     # If the feedback is correct, break the loop and return the code
                     break
 
@@ -118,7 +156,7 @@ class LLMCoder:
         """
         return os.path.join(get_conversations_dir(create=True), f"{datetime.now()}.jsonl")
 
-    def _add_message(self, role: str, model: str = 'gpt-3.5-turbo', message: str | None = None) -> None:
+    def _add_message(self, role: str, model: str = 'gpt-3.5-turbo', message: str | None = None, temperature: float = 0.7, n: int = 14) -> None:
         """
         Add a message to the messages list
 
@@ -130,12 +168,25 @@ class LLMCoder:
             The model to use for the completion, by default 'gpt-3.5-turbo'
         message : str, optional
             The message to add, by default None
+        temperature : float, optional
+            The temperature to use for the completion, by default 0.7
+        n : int, optional
+            The number of choices to generate, by default 14
         """
         # If the user is the assistant, generate a response
         if role == "assistant" and message is None:
-            chat_completion = self.client.chat.completions.create(messages=self.messages, model=model, temperature=self.temperature)  # type: ignore
+            chat_completions = self.client.chat.completions.create(messages=self.messages, model=model, temperature=temperature, n=n)  # type: ignore
 
-            message = chat_completion.choices[0].message.content
+            messages = [choice.message.content for choice in chat_completions.choices]
+
+            # Get the best message according to the scoring model
+            scores = self.completion_score_tokenizer(messages, padding=True, truncation=True, return_tensors="pt")
+
+            with torch.no_grad():
+                scores = self.completion_score_model(**scores.to(self.device)).logits[:, 0].cpu().numpy()  # The first class captures the code quality, the second one the need for comments (not used here))
+
+            # Get the best message according to the scoring model
+            message = messages[scores.argmax()]
 
         self.messages.append({
             "role": role,
@@ -174,7 +225,7 @@ class LLMCoder:
         self.analyzer_pass_history.append([results['pass'] for results in analyzer_results])
         self.analyzer_message_history.append([results['message'] for results in analyzer_results])
 
-    def complete_first(self, code: str) -> dict:
+    def complete_first(self, code: str, temperature: float = 0.7, n: int = 14) -> str:
         """
         Run the first completion of the LLMCoder without any feedback
 
@@ -182,11 +233,15 @@ class LLMCoder:
         ----------
         code : str
             The code to complete
+        temperature : float, optional
+            The temperature to use for the completion, by default 0.7
+        n : int, optional
+            The number of choices to generate, by default 14
 
         Returns
         -------
-        dict
-            The message of the assistant
+        str
+            The completed code
         """
         self._reset_loop()
 
@@ -194,14 +249,21 @@ class LLMCoder:
         self._add_message("user", message=code)
 
         # First completion: do it changing the output format, i.e. using the fine-tuned model
-        self._add_message("assistant", model=self.model_first)
+        self._add_message("assistant", model=self.model_first, temperature=temperature, n=n)
 
         # Return the last message (the completion)
         return self.messages[-1]["content"]
 
-    def feedback_step(self) -> bool:
+    def feedback_step(self, temperature: float = 0.7, n: int = 14) -> bool:
         """
         Run the feedback step of the LLMCoder feedback loop
+
+        Parameters
+        ----------
+        temperature : float, optional
+            The temperature to use for the completion, by default 0.7
+        n : int, optional
+            The number of choices to generate, by default 14
 
         Returns
         -------
@@ -233,6 +295,6 @@ class LLMCoder:
         error_prompt = '[INST]\nConsider the following in your next completion:\n[ANALYSIS]\n' + '\n'.join([results['message'] for results in analyzer_results if not results['pass']]) + '\n[/ANALYSIS]\nSeamlessly complete the following code:\n[/INST]\n'
 
         self._add_message("user", message=error_prompt + self.messages[1]['content'])
-        self._add_message("assistant")
+        self._add_message("assistant", model=self.model_first, temperature=temperature, n=n)  # model_first works quite good here
 
         return False
